708fb22af46d030332378f5a1c73f19014a49792
Evan Klein <Evan.Klein@Milliman.com>
2022-08-02 09:59:36 -0400

    UI improvements and bug fix

--- src/damage_system.rs
+++ src/damage_system.rs
@@ -1,5 +1,5 @@
 use specs::prelude::*;
-use super::{CombatStats, SufferDamage};
+use super::{CombatStats, SufferDamage, Player, Name, gamelog::GameLog};
 
 pub struct DamageSystem {}
 
@@ -16,18 +16,33 @@ impl<'a> System<'a> for DamageSystem {
     }
 }
 
-pub fn delete_the_dead(ecs: &mut World) {
-    let mut dead: Vec<Entity> = Vec::new();
-    //Using a scope to make the borrow checker happy
+pub fn delete_the_dead(ecs : &mut World) {
+    let mut dead : Vec<Entity> = Vec::new();
+    // Using a scope to make the borrow checker happy
     {
         let combat_stats = ecs.read_storage::<CombatStats>();
+        let players = ecs.read_storage::<Player>();
+        let names = ecs.read_storage::<Name>();
         let entities = ecs.entities();
+        let mut log = ecs.write_resource::<GameLog>();
         for (entity, stats) in (&entities, &combat_stats).join() {
-            if stats.hp < 1 { dead.push(entity); }
+            if stats.hp < 1 { 
+                let player = players.get(entity);
+                match player {
+                    None => {
+                        let victim_name = names.get(entity);
+                        if let Some(victim_name) = victim_name {
+                            log.entries.push(format!("{} is dead", &victim_name.name));
+                        }
+                        dead.push(entity)
+                    }
+                    Some(_) => log.entries.push(format!("You are dead"))
+                }
+            }
         }
     }
 
     for victim in dead {
         ecs.delete_entity(victim).expect("Unable to delete");
-    }
+    }    
 }
